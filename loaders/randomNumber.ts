e42ba2bb2f18ba5ec2577584d7f4c54f9abebbbc
Marcos Candeia <marrcooos@gmail.com>
2025-03-26 10:39:21 -0300

    Remove loaders
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- loaders/randomNumber.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @name random_number
- * @description Returns a random number between 0 and 1
- */
-export default function randomNumber() {
-  return Math.random();
-}
--- main.ts
+++ main.ts
@@ -1,15 +1,36 @@
-import { Deco } from "@deco/deco";
+// deno-lint-ignore-file no-explicit-any
+import { Deco } from '@deco/deco';
+import { fromEndpoint } from "@deco/deco/engine";
 import { mcpServer } from "@deco/mcp";
 import { Hono } from "@hono/hono";
-import manifest, { Manifest } from "./manifest.gen.ts";
+import manifest from "./manifest.gen.ts";
 
 const app = new Hono();
-const deco = await Deco.init<Manifest>({
-  manifest,
-});
 const envPort = Deno.env.get("PORT");
 
-app.use("/*", mcpServer(deco));
-app.all("/*", async (c) => c.res = await deco.fetch(c.req.raw));
+const contexts: Record<string, Promise<Deco>> = {};
+
+const decoInstance = (url: string): Promise<Deco> => {
+  let mcpId = new URL(url).searchParams.get("install");
+  if (!mcpId) {
+    mcpId = "default"
+  }
+  contexts[mcpId] ??= Deco.init<any>({
+    manifest,
+    decofile: URL.canParse(mcpId) ? fromEndpoint(mcpId) : undefined,
+  });
+
+  return contexts[mcpId];
+}
+
+app.use("/*", async (ctx, next) => {
+  const instance = await decoInstance(ctx.req.url);
+  await mcpServer(instance as any)(ctx, next);
+});
+
+app.all("/*", async (c) => {
+  const instance = await decoInstance(c.req.url);
+  c.res = await instance.fetch(c.req.raw);
+});
 
 Deno.serve({ handler: app.fetch, port: envPort ? +envPort : 8000 });
