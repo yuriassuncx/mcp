1061b4894e2b4fc5f23a9cebdc9ec2ad64a07634
Marcos Candeia <marrcooos@gmail.com>
2025-03-26 21:43:22 -0300

    Use LRU cache
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- registry.ts
+++ registry.ts
@@ -3,15 +3,18 @@ import { Deco, DecoOptions } from "@deco/deco";
 import { installStorage } from "site/apps/site.ts";
 import manifest, { Manifest } from "site/manifest.gen.ts";
 import { mcpServer } from "@deco/mcp";
+import { LRUCache } from "lru-cache";
 
 export interface MCPInstance {
   deco: Deco<Manifest>;
   server: ReturnType<typeof mcpServer<Manifest>>;
 }
-const contexts: Record<
-  string,
-  Promise<MCPInstance>
-> = {};
+
+const contexts = new LRUCache<string, Promise<MCPInstance>>({
+  max: 100,
+  ttl: 1000 * 60 * 60,
+  updateAgeOnGet: true,
+});
 
 export const decoInstance = async (
   installId?: string,
@@ -32,22 +35,26 @@ export const decoInstance = async (
     ? `/apps/${appName}/${installId}`
     : undefined;
 
-  contexts[installId] ??= Deco.init<Manifest>({
-    manifest,
-    decofile,
-  }).then((deco) => ({
-    deco,
-    server: mcpServer<Manifest>(
+  let instance = contexts.get(installId);
+  if (!instance) {
+    instance = Deco.init<Manifest>({
+      manifest,
+      decofile,
+    }).then((deco) => ({
       deco,
-      appName && installId
-        ? {
-          basePath,
-        }
-        : undefined,
-    ),
-  }));
+      server: mcpServer<Manifest>(
+        deco,
+        appName && installId
+          ? {
+            basePath,
+          }
+          : undefined,
+      ),
+    }));
+    contexts.set(installId, instance);
+  }
 
-  return contexts[installId];
+  return instance;
 };
 export const { deco: MCP_REGISTRY, server: MCP_SERVER } =
   (await decoInstance())!;
